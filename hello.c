98f2e41e7d05ab8d19b0c5d160b104b725c8fd93
Alexander Smirnov <asmirnov@ilbers.de>
2018-01-22 18:36:15 +0000

    libhello: First implementation
    This is a sample shared library that can be used as a skeleton.
    
    Signed-off-by: Alexander Smirnov <asmirnov@ilbers.de>
    Acked-by: Baurzhan Ismagulov <ibr@ilbers.de>

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2017-2018 ilbers GmbH
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include "hello.h"
+
+int say_hello(void)
+{
+	printf("Hello, world!\n");
+	return 0;
+}
---
+++ hello.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2017-2018 ilbers GmbH
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+
+int say_hello(void);
+
+#endif
